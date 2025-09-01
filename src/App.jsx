80c7630a3c5f14169a15d6c812f7fe57c84075b3
Juan Sebastian Pabon Mojica <nova159712@gmail.com>
2025-09-01 16:31:44 -0500

    Añade el json-server para la fakeApi y el Reducer para controlar las acciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-/* import { useState } from "react"; */
+import { useState, useEffect, useReducer } from "react";
 import DateCounter from "./DateCounter";
 import Header from "./Header";
 import MainContent from "./MainContent";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+
 function App() {
+  const initialState = {
+    questions: [],
+    //loading, errror, ready, active, finished
+    status: "loading",
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch({ type: "dataReceived", payload: data });
+      })
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className='app'>
       <Header />
